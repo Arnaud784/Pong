ee00b08cf7ff1cf4f0bb3fd17d49534fc0dc27b4
Sebastouf <raph.en223@gmail.com>
2019-04-10 13:35:49 +0200

    hi

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -2,18 +2,16 @@ package com.example.pong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        PongView pongview = new PongView(this, metrics.widthPixels, metrics.heightPixels);*/
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         PongView pongview = new PongView(this);
 
         setContentView(pongview);
