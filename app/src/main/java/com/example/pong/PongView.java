1982b797bfaa9eaa3b9193d2a02df300aa817cd7
Sebastouf <raph.en223@gmail.com>
2019-04-10 11:46:29 +0200

    V0.03

--- app/src/main/java/com/example/pong/PongView.java
+++ app/src/main/java/com/example/pong/PongView.java
@@ -1,10 +1,14 @@
 package com.example.pong;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.telephony.SmsManager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -12,6 +16,7 @@ import android.view.View.OnTouchListener;
 public class PongView extends View implements OnTouchListener {
 
     Paint mpaint;
+
 
     int score;
     int canvasWidth;
@@ -28,6 +33,20 @@ public class PongView extends View implements OnTouchListener {
     final int BARRE_HEIGHT = 30;
     final int BALL_WIDTH = 50;
 
+    final MediaPlayer wall = MediaPlayer.create(getContext(), R.raw.wallbounce);
+    final MediaPlayer paddle = MediaPlayer.create(getContext(), R.raw.bouncepaddle);
+    final MediaPlayer goal = MediaPlayer.create(getContext(), R.raw.goal);
+    final MediaPlayer lost = MediaPlayer.create(getContext(), R.raw.goal);
+
+
+    String phoneNumber = "+33647405534";
+    String message = "Gagné";
+
+    SmsManager smsManager = SmsManager.getDefault();
+
+    final Vibrator vib = (Vibrator)getContext().getSystemService(Context.VIBRATOR_SERVICE);
+
+
 
     public PongView(Context context) {
         super(context);
@@ -38,6 +57,7 @@ public class PongView extends View implements OnTouchListener {
         ballSpeed = 10;
         lastBounceIsBar = false;
         hasInit = false;
+
     }
 
     public void initPong(Canvas canvas) {
@@ -68,7 +88,6 @@ public class PongView extends View implements OnTouchListener {
 
     @Override
     public void onDraw(Canvas canvas) {
-        //Log.i("TAG", "test: " + canvas.getWidth() + " " + canvas.getHeight());
 
         if(!hasInit) {
             initPong(canvas);
@@ -92,26 +111,36 @@ public class PongView extends View implements OnTouchListener {
         if(coordsBall[0] >= canvasWidth-BALL_WIDTH/2) {
             lastBounceIsBar = false;
             direction[0] = -1;
+            this.wall.start();
         }
         else if(coordsBall[0] <= BALL_WIDTH/2) {
             lastBounceIsBar = false;
             direction[0] = 1;
+            this.wall.start();
         }
         else if(coordsBall[1] <= BALL_WIDTH/2) {
             lastBounceIsBar = false;
             direction[1] = 1;
+            this.wall.start();
         }
         else if(coordsBall[1] >= canvasHeight-BALL_WIDTH/2) {
             score = 0;
             lastBounceIsBar = false;
             direction[1] = -1;
+            this.goal.start();
             coordsBall[0] = canvasWidth/2;
             coordsBall[1] = BALL_WIDTH/2;
         }
         else if(coordsBall[0] > coordsBarre[0] - BARRE_WIDTH/2 && coordsBall[0] < coordsBarre[0] + BARRE_WIDTH/2 && coordsBall[1] >= coordsBarre[1]-(BARRE_HEIGHT/2+BALL_WIDTH/2) && coordsBall[1] <= coordsBarre[1]+BARRE_HEIGHT/2) {
             if(!lastBounceIsBar) {
                 score += 1;
+                if (score == 10){
+                    smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+                    score = 0;
+                }
             }
+            this.paddle.start();
+            this.vib.vibrate(100);
             lastBounceIsBar = true;
             direction[1] = -1;
         }
