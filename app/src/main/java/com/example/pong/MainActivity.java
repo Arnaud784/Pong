0793e5eec506c2fe6aec07506e874f07d5ddf015
Sebastouf <raph.en223@gmail.com>
2019-04-11 11:08:54 +0200

    v0.05

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.WindowManager;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -17,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
-
         PongView pongview = new PongView(this);
         setContentView(pongview);
+
     }
+
+
+
 }
