f8067f953e7e9c325867554dc7abf76319cfeae9
Unknown <arnaud.lieveaux@gmail.com>
2019-04-11 14:27:04 +0200

    Version production

---
+++ app/src/main/java/com/notresiterebound/rebound/MainActivity.java
@@ -0,0 +1,33 @@
+package com.notresiterebound.rebound;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        PongView pongview = new PongView(this);
+        setContentView(pongview);
+
+    }
+
+
+
+}
